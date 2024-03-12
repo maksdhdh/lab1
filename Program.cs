1e90c110f251cf9cac938989ca4615d29315c1bc
Maksym Yarmola <152811314+maksdhdh@users.noreply.github.com>
2024-03-12 20:26:40 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        List<string> keys = new List<string>() { "key1", "key2", "key3" };
-        List<int> values = new List<int>() { 1, 2, 3 };
+        var numbers = Enumerable.Range(-10, 11);
 
-        Dictionary<string, List<int>> dictionary = new Dictionary<string, List<int>>();
-
-        for (int i = 0; i < keys.Count; i++)
+        bool foundPositive = false;
+        foreach (var number in numbers)
         {
-            dictionary[keys[i]] = new List<int>();
-            dictionary[keys[i]].Add(values[i]);
+            if (number > 0)
+            {
+                Console.WriteLine(number);
+                foundPositive = true;
+                break;
+            }
         }
 
-        Console.WriteLine("Dictionary:");
-        foreach (var pair in dictionary)
+        if (!foundPositive)
         {
-            Console.WriteLine($"{pair.Key}: [{string.Join(", ", pair.Value)}]");
+            Console.WriteLine(0);
         }
-        string json = JsonSerializer.Serialize(dictionary, new JsonSerializerOptions { WriteIndented = true });
-        File.WriteAllText("dictionary.json", json);
-
-        Console.WriteLine("Dictionary saved to dictionary.json");
-
-        Console.ReadLine();
     }
 }
-
